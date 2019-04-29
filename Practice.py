ae7ff354c8b52e9c228746d65d7c211c49f12c39
dml7276 <49231342+dml7276@users.noreply.github.com>
2019-04-29 21:47:51 +0900

    파이썬 과제

---
+++ Practice.py
@@ -0,0 +1,4 @@
+a = '1 2 3 4 5 6 7 8 9 10'
+b = a.split()
+for num in b : 
+    print(num)
---
+++ Practice2.py
@@ -0,0 +1,8 @@
+a = 'i love you do you love me?'
+my_dict = {'i': 1, 'love': 2, 'you': 2, 'do': 1, 'me?': 1}
+print(my_dict['i'])
+print(my_dict['love'])
+print(my_dict['you'])
+print(my_dict['do'])
+print(my_dict['me?'])
+print(my_dict)
